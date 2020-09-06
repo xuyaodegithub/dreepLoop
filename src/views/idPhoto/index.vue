daa2c005db9e2ccb687d225b3cf58e9da6a2ae41
supYan <>
2020-09-06 14:27:36 +0800

    -9-6

--- src/views/idPhoto/index.vue
+++ src/views/idPhoto/index.vue
@@ -4,48 +4,46 @@
     </div>
 </template>
 <script>
-    import {mapGetters,mapActions} from 'vuex'
+    import {mapGetters, mapActions} from 'vuex'
     import headerSub from '@/components/header/index.vue'
     import {getToken, getSecImgs, setSecImgs} from "../../utils/auth";
-    import { initSmallTag } from '@/utils'
+    import {initSmallTag} from '@/utils'
     import {Message, Notification} from 'element-ui'
+
     export default {
         name: 'HelloWorld',
         data() {
-            return {
-
-            }
+            return {}
         },
-        filters: {
-
-        },
+        filters: {},
         watch: {},
         mounted() {
-            window.showloginDialog=this.showloginDialog;
-            window.Notification=this.showNotification;
-            window.initSmallTag=initSmallTag;
+            window.showloginDialog = this.showloginDialog;
+            window.Notification = this.showNotification;
+            window.initSmallTag = initSmallTag;
+            window.confirm = this.$confirm;
             // var showloginDialog= this.showLoginDilogAction()
             // $('.hisimgs').niceScroll({cursorcolor :'#999999',boxzoom:true});
         },
         destroyed() {
         },
         computed: {
-            ...mapGetters( [] ),
+            ...mapGetters([]),
         },
         components: {
             headerSub,
         },
         methods: {
-            ...mapActions( [
-                'userGetscribe','showLoginDilogAction'
-            ] ),
-            showNotification(str){
-                Notification( {
+            ...mapActions([
+                'userGetscribe', 'showLoginDilogAction'
+            ]),
+            showNotification(str) {
+                Notification({
                     type: 'error',
                     message: str
-                } )
+                })
             },
-            showloginDialog(){
+            showloginDialog() {
                 this.showLoginDilogAction()
             },
             upLoadimg() {//点击上传
@@ -57,21 +55,21 @@
             },
             stopPrevent() {//阻止游览器默认打开图片
                 let _self = this
-                document.addEventListener( "drop", function (e) {  //拖离
+                document.addEventListener("drop", function (e) {  //拖离
                     e.preventDefault();
-                } );
-                document.addEventListener( "dragleave", function (e) {  //拖后放
+                });
+                document.addEventListener("dragleave", function (e) {  //拖后放
                     e.preventDefault();
-                } );
-                document.addEventListener( "dragenter", function (e) {  //拖进
+                });
+                document.addEventListener("dragenter", function (e) {  //拖进
                     e.preventDefault();
-                } );
-                document.addEventListener( "dragover", function (e) {  //拖来拖去
+                });
+                document.addEventListener("dragover", function (e) {  //拖来拖去
                     e.preventDefault();
-                } );
-                let oDrops = document.getElementsByClassName( 'drops' );
+                });
+                let oDrops = document.getElementsByClassName('drops');
                 for (let i = 0; i < oDrops.length; i++) {
-                    oDrops[i].addEventListener( "drop", function (e) {
+                    oDrops[i].addEventListener("drop", function (e) {
                         e.preventDefault();
                         e.stopPropagation();
                         // e.cancelable=true
@@ -79,22 +77,22 @@
                         // console.log(files)
                         if (!files.length) return;
                         if (!getToken() && files.length > 1) {
-                            console.log( files )
-                            _self.$message( {type: 'error', message: '登录后可批量上传'} )
+                            console.log(files)
+                            _self.$message({type: 'error', message: '登录后可批量上传'})
                             return
                         }
                         _self.toscroll();
                         for (let i = 0; i < files.length; i++) {
-                            console.log( files )
-                            _self.files.unshift( {
+                            console.log(files)
+                            _self.files.unshift({
                                 url: files[i],
-                                name: parseInt( Math.random() * 100000000000 ),
+                                name: parseInt(Math.random() * 100000000000),
                                 type: 'file',
                                 filename: files[i].name
-                            } )
+                            })
                         }
-                        _self.imgUrlss( files )
-                    } )
+                        _self.imgUrlss(files)
+                    })
                 }
             },
 
@@ -231,16 +229,16 @@
             flex: 1;
             padding: 40px 0 40px 50px;
             background-color: #fff;
-            .sizeClass{
+            .sizeClass {
                 font-size: 34px;
                 color: #333;
                 font-weight: bold;
-                .el-input{
+                .el-input {
                     width: 400px;
                     margin-left: 60px;
                 }
             }
-           & > label{
+            & > label {
                 position: relative;
                 text-indent: 1em;
                 display: block;
@@ -249,8 +247,8 @@
                 color: #333;
                 margin-bottom: 20px;
                 line-height: 1;
-               font-weight: bold;
-                &:before{
+                font-weight: bold;
+                &:before {
                     position: absolute;
                     content: '';
                     width: 6px;
@@ -260,33 +258,33 @@
                     background-color: #e82255;
                 }
             }
-            .cList{
+            .cList {
                 flex-wrap: wrap;
-                & > div{
+                & > div {
                     margin: 0 20px 30px 0;
                 }
-                .its{
+                .its {
                     position: relative;
                     width: 235px;
                     height: 235px;
                     box-shadow: 0 0 5px #9e9e9e;
                     border-radius: 5px;
-                    &>div{
+                    & > div {
                         position: absolute;
                         left: 50%;
                         top: 50%;
-                        transform: translate(-50%,-50%);
+                        transform: translate(-50%, -50%);
                         overflow: hidden;
-                        img{
+                        img {
                             position: absolute;
                             height: 100%;
                             left: 50%;
                             top: 50%;
-                            transform: translate(-50%,-50%);
+                            transform: translate(-50%, -50%);
                         }
                     }
                 }
-                p{
+                p {
                     font-size: 14px;
                     color: #000;
                     line-height: 44px;
